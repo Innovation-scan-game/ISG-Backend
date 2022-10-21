9c1f0af71baa719430429d3844240573372dba69
Jurek Baumann <jurekbaumann@me.com>
2022-10-21 13:20:46 +0200

    Add tests

---
+++ InnovationGameTests/MockBindingContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace InnovationGameTests;
+
+public class MockBindingContext : BindingContext
+{
+    public MockBindingContext(Dictionary<string, object?> bindingData)
+    {
+        BindingData = bindingData;
+    }
+    public override IReadOnlyDictionary<string, object?> BindingData { get;  }
+}
---
+++ InnovationGameTests/MockBindingData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace InnovationGameTests;
+
+public class MockBindingMetadata : BindingMetadata
+{
+    public MockBindingMetadata(string type, BindingDirection direction)
+    {
+        Type = type;
+        Direction = direction;
+    }
+
+    public override string Name { get; }
+    public override string Type { get; }
+
+    public override BindingDirection Direction { get; }
+}
