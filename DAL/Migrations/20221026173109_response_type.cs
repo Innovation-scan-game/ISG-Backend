4d7163ee2a78deb91d00885a970dd291877b5a64
Jurek Baumann <jurekbaumann@me.com>
2022-10-26 23:54:42 +0200

    Implement conclusions

---
+++ DAL/Migrations/20221026173109_response_type.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class response_type : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ResponseType",
+                table: "SessionResponses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResponseType",
+                table: "SessionResponses");
+        }
+    }
+}
--- Domain/Models/SessionResponse.cs
+++ Domain/Models/SessionResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace Domain.Models;
+﻿using Domain.Enums;
+
+namespace Domain.Models;
 
 public class SessionResponse
 {
@@ -8,4 +10,5 @@ public class SessionResponse
     public User User { get; set; }
     public string Response { get; set; } = "";
     public DateTime CreatedAt { get; set; }
+    public SessionResponseType ResponseType { get; set; } = SessionResponseType.Answer;
 }
